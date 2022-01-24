9ed01bfab7c22c9bd4ded5ef94ecb57ed52bf819
mrexhepi <muharrem.rexhepi@poviolabs.com>
2022-01-24 14:37:11 +0100

    fix:removed cdk example(failed)

--- cdk-example/bin/cdk-example.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { CdkExampleStack } from '../lib/cdk-example-stack';
-
-const app = new cdk.App();
-new CdkExampleStack(app, 'CdkExampleStack');
